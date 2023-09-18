af4df15ab7d2f8aae4168a40959cae35a5a61da3
Vitaly <vitaly.varela@hotmail.com>
2023-09-18 17:06:34 -0600

    endsWith

---
+++ codewars/endsWith.js
@@ -0,0 +1,8 @@
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// endsWith sirve para saber si termina de la misma forman el elemento  string
+
+function solution(str, ending){
+    return str.endsWith(ending)
+  }
+  console.log( solution('abcde', 'cde'));
